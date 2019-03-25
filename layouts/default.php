1bd6abbf7fb8f6bdd3d81f8e4d6dfc920dc25793
Joshua Britt <joshua.britt@pm-international.com>
2019-03-25 17:33:39 -0500

    Added styles to layout file.

--- layouts/default.php
+++ layouts/default.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
     <title>Jobs</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" />
+    <link href="<?= path(['STYLES', 'default.css']); ?>" rel="stylesheet" />
   </head>
   <body>
     <main>
